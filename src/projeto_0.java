914ff50bc58367325aff3f536407797c3a278f63
wendell <wendelllopes2@yahoo.com.br>
2020-04-21 22:26:01 -0300

    Mensagem Explicativa 2

--- src/projeto_0.java
+++ src/projeto_0.java
@@ -2,7 +2,8 @@
 public class projeto_0 {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro Projetohhhh");
+		System.out.println("Primeiro Projeto");
+		System.out.println("Segundo Projeto");
 
 	}
 
